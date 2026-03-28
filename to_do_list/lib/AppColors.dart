06255dfd2007e362032eabcbddfcfb9537df510a
Birang Pawala <birangmodi9327@gmail.com>
2026-03-28 21:47:19 +0530

    color defined

---
+++ to_do_list/lib/AppColors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xFF00A86B); // your custom green
+  static const secondary = Color(0xFFFFC107);
+  static const background = Color(0xFFF5F5F5);
+}
