28e18b5082e0bf3ecc130f31685b513403e9c636
Birang Pawala <birangmodi9327@gmail.com>
2026-03-29 16:49:48 +0530

    basic changes

--- to_do_list/test/widget_test.dart
+++ to_do_list/test/widget_test.dart
@@ -10,12 +10,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:to_do_list/main.dart';
+import 'package:to_do_list/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     final prefs = await SharedPreferences.getInstance();
-    await tester.pumpWidget(MyApp(prefs: prefs));
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
